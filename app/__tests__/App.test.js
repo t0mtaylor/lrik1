201c4efc21eb41344eb0c5fee25b01ea5ca3a6d2
tom_taylor <me@tommytaylor.co.uk>
2018-10-04 09:53:23 +0100

    introduce snapshots to App test

--- app/__tests__/App.test.js
+++ app/__tests__/App.test.js
@@ -1,9 +1,17 @@
 
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
 
 
-it('renders App without crashing', () => { 
-  shallow(<App />); 
+import App from '../src/App';
+
+
+describe('App', () => {
+	
+	it('renders App without crashing', () => { 
+		const app = shallow(<App />); 
+								
+		expect(app).toMatchSnapshot();
+	});
+			
 });
